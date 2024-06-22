50c635e726d64fef0575eaddcb07a606a3b45103
kremec <klemen.remec@gmail.com>
2024-06-22 19:46:23 +0200

    Quartz sync: Jun 22, 2024, 7:46 PM

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -1,7 +1,5 @@
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 import style from "./styles/footer.scss"
-import { version } from "../../package.json"
-import { i18n } from "../i18n"
 
 interface Options {
   links: Record<string, string>
@@ -15,8 +13,7 @@ export default ((opts?: Options) => {
       <footer class={`${displayClass ?? ""}`}>
         <hr />
         <p>
-          {i18n(cfg.locale).components.footer.createdWith}{" "}
-          <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
+          <a>Created by SubByte</a>
         </p>
         <ul>
           {Object.entries(links).map(([text, link]) => (
