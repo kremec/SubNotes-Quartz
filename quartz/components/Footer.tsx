6dbe5432e0d721ac9c3053d9a05fe6078b03beec
kremec <klemen.remec@gmail.com>
2024-06-22 20:11:18 +0200

    Quartz sync: Jun 22, 2024, 8:11 PM

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -7,22 +7,8 @@ interface Options {
 
 export default ((opts?: Options) => {
   const Footer: QuartzComponent = ({ displayClass, cfg }: QuartzComponentProps) => {
-    const year = new Date().getFullYear()
-    const links = opts?.links ?? []
     return (
-      <footer class={`${displayClass ?? ""}`}>
-        <hr />
-        <p>
-          <a>Created by SubByte</a>
-        </p>
-        <ul>
-          {Object.entries(links).map(([text, link]) => (
-            <li>
-              <a href={link}>{text}</a>
-            </li>
-          ))}
-        </ul>
-      </footer>
+      <></>
     )
   }
 
